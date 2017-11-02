92ff3f669ce072c13d623378431f1bcd6f6d122c
Viet Nguyen <vietjul@gmail.com>
2017-11-02 12:18:39 -0400

    list all diff done. sort or pick min diff

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,8 +16,26 @@ function apiHandler(app) {
         } */
 
         var newScores = req.body.scores;
+        var allDiff = [];
 
-        for (i = 0; i < newScores.length; i++) {
+        for (i = 0; i < friends.length; i++) {
+            console.log('\n===========\n' + friends[i].name + ' scores: ');
+
+            allDiff[friends[i].name] = 0;
+
+            for (j = 0; j < friends[i].scores.length; j++) {
+                console.log('\nQuestion ' + (j + 1) + ': ' + friends[i].scores[j]);
+                console.log('Your answer: ' + newScores[j]);
+
+                var diff = Math.abs(newScores[j] - friends[i].scores[j]);
+                
+                console.log('Your difference is: ', diff);
+
+                allDiff[friends[i].name] += diff;
+            }
+        }
+
+        /* for (i = 0; i < newScores.length; i++) {
             console.log('\n==============\nQuestion ' + (i + 1) + '\n');
             console.log('You chose: ', newScores[i]);
 
@@ -27,7 +45,10 @@ function apiHandler(app) {
 
                 console.log('Your difference is: ', diff);
             }
-        }
+        } */
+
+        console.log('\n===========\n');
+        console.log(allDiff);
 
         friends.push(req.body);
         res.json(true);
