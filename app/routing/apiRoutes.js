2444726972db3b619e0e349bd247568d2d0a39fe
Viet Nguyen <vietjul@gmail.com>
2017-11-02 14:27:17 -0400

    add google jquery, send best match. next: display on page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,51 +11,54 @@ function apiHandler(app) {
     // add new friend
     app.post('/api/friends', (req, res) => {
 
+        // handle compatibility
         var newScores = req.body.scores;
         var friendDiff = {};
 
+        // check all friends
         for (i = 0; i < friends.length; i++) {
             console.log('\n===========\n' + friends[i].name + ' scores: ');
+            
             var totalDiff = 0;
 
+            // check all answers
             for (j = 0; j < friends[i].scores.length; j++) {
                 console.log('\nQuestion ' + (j + 1) + ': ' + friends[i].scores[j]);
                 console.log('Your answer: ' + newScores[j]);
 
+                // get the difference between new answer and friend's answer
                 var diff = Math.abs(newScores[j] - friends[i].scores[j]);
                 
                 console.log('Your difference for this question is: ', diff);
 
+                // add all differences
                 totalDiff += diff;
             }
 
             console.log('Your total difference is: ', totalDiff);
 
+            // store friends' names and differences in an object by difference order (if same difference, latter name displayed)
             friendDiff[totalDiff] = friends[i].name;
         }
 
+        // log list of all friends and differences
         console.log(friendDiff);
-        console.log(friendDiff[0]);
 
-        // var obj = { 0: 'a', 1: 'b', 2: 'c' };
-        // console.log(Object.keys(obj)); // console: ['0', '1', '2']
+        // name of best match
+        console.log('\n========\nYour best friend is: ', friendDiff[Object.keys(friendDiff)[0]]);
 
-        // points.sort(function(a, b){return a-b});
+        var friendName = friendDiff[Object.keys(friendDiff)[0]];
+        var friendInfo = {};
 
-        /* for (i = 0; i < newScores.length; i++) {
-            console.log('\n==============\nQuestion ' + (i + 1) + '\n');
-            console.log('You chose: ', newScores[i]);
-
-            for (j = 0; j < friends.length; j++) {
-                console.log(friends[j].name + ' chose: ' + friends[j].scores[i]);
-                var diff = Math.abs(newScores[i] - friends[j].scores[i]);
-
-                console.log('Your difference is: ', diff);
+        for (i = 0; i < friends.length; i++) {
+            if (friendName === friends[i].name) {
+                friendInfo.name = friends[i].name;
+                friendInfo.photo = friends[i].photo;
             }
-        } */
-         
+        }
+
         friends.push(req.body);
-        res.json(true);
+        res.json(friendInfo);
     });
 }
 
