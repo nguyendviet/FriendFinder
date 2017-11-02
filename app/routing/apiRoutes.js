593d87d21d52c1aba515ac17eb5f857f5c36b234
Viet Nguyen <vietjul@gmail.com>
2017-11-02 11:09:28 -0400

    listed all answers by each friend. next: compare answers question by question

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,18 +4,26 @@ function apiHandler(app) {
 
     // show list of current friends
     app.get('/api/friends', (req, res) => {
-        console.log(req);
-        console.log(res);
         // send friends json
         res.json(friends);
     });
 
     // add new friend
     app.post('/api/friends', (req, res) => {
+
+        var newScores = req.body.scores;
+
+        for (i = 0; i < newScores.length; i++) {
+            console.log('\n==============\nQuestion ' + (i + 1) + '\n');
+            console.log('You chose: ', newScores[i]);
+
+            for (j = 0; j < friends.length; j++) {
+                console.log(friends[j].name + ' chose: ' + friends[j].scores[i]);
+            }
+        }
+
         friends.push(req.body);
         res.json(true);
-
-        // compatibility logic
     });
 }
 
