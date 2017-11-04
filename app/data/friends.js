3c669108167d6314990329c82152f88f6d19c663
Viet Nguyen <vietjul@gmail.com>
2017-11-04 17:45:33 -0400

    modal works. need: questions, friends data, photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
-        'name': 'James',
-        'photo': '',
+        'name': 'Yoda',
+        'photo': 'https://images.rapgenius.com/fbbd7d56af82dc1ee0242f4d1e30746a.355x400x1.jpg',
         'scores': [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
     },
     {
@@ -10,13 +10,13 @@ var friends = [
         'scores': [5, 4, 3, 2, 1, 5, 4, 3, 2, 1]
     },
     {
-        'name': 'Rolando',
-        'photo': '',
+        'name': 'Rocky Balboa',
+        'photo': 'https://images.moviepilot.com/images/c_limit,q_auto:good,w_600/bjj3uax-5-reasons-why-we-love-rocky-balboa-jpeg-144195/5-reasons-why-we-love-rocky-balboa.jpg',
         'scores': [2, 4, 2, 4, 2, 4, 2, 4, 2, 4]
     },
     {
-        'name': 'Viet',
-        'photo': '',
+        'name': 'The Terminator',
+        'photo': 'https://upload.wikimedia.org/wikipedia/en/b/b9/Terminator-2-judgement-day.jpg',
         'scores': [1, 3, 5, 1, 3, 5, 1, 3, 5, 1]
     }
 ];
