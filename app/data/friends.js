e2187a7d2d1743b0890065951eb03e67f90cff0a
Viet Nguyen <vietjul@gmail.com>
2017-11-04 21:45:54 -0400

    update decor

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,22 @@ var friends = [
     {
         'name': 'Yoda',
         'photo': 'https://images.rapgenius.com/fbbd7d56af82dc1ee0242f4d1e30746a.355x400x1.jpg',
-        'scores': [4, 5, 1, 4, 5, 1, 2, 3, 5, 2]
+        'scores': ['4', '5', '1', '4', '5', '1', '2', '3', '5', '2']
     },
     {
         'name': 'Mystique',
         'photo': 'http://static5.businessinsider.com/image/55a3f891ecad04195f3fc076-480/x-men-days-of-future-past-mystique-jennifer-lawrence.png',
-        'scores': [1, 1, 5, 3, 2, 3, 4, 2, 1, 5]
+        'scores': ['1', '1', '5', '3', '2', '3', '4', '2', '1', '5']
     },
     {
         'name': 'Black Widow',
         'photo': 'https://images.moviepilot.com/image/upload/c_fill,h_470,q_auto:good,w_620/dgn7zs771xwx4tsjnnwl.jpg',
-        'scores': [3, 2, 3, 5, 3, 4, 2, 3, 2, 4]
+        'scores': ['3', '2', '3', '5', '3', '4', '2', '3', '2', '4']
     },
     {
         'name': 'The Terminator',
         'photo': 'https://upload.wikimedia.org/wikipedia/en/b/b9/Terminator-2-judgement-day.jpg',
-        'scores': [5, 4, 4, 1, 4, 5, 5, 5, 4, 1]
+        'scores': ['5', '4', '4', '1', '4', '5', '5', '5', '4', '1']
     }
 ];
 
